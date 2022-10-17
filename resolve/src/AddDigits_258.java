aba7fc6539a751811145bfa3f992fcc16e892d35
liuguangqiang <liuguangqiang@baidu.com>
2022-10-17 11:35:17 +0800

    AddDigits_258

---
+++ resolve/src/AddDigits_258.java
@@ -0,0 +1,13 @@
+public class AddDigits_258 {
+    public int addDigits(int num) {
+        do {
+            int sum = 0;
+            while (num != 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+        } while (num >= 10);
+        return num;
+    }
+}
