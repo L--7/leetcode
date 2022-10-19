aa27180daea522ffe8893b8ca3f5648ed3f6f538
L7 <1525804307@qq.com>
2022-10-19 22:10:08 +0800

    374 猜数字大小

---
+++ resolve/src/GuessNumber_374.java
@@ -0,0 +1,17 @@
+public class GuessNumber_374 {
+    public int guessNumber(int n) {
+        int low = 0, high = n;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int result = guess(mid);
+            if ( result == 0) {
+                return mid;
+            } else if(result == 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
+}
