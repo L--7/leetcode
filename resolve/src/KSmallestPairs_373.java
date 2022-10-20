6bdb4dd5367936014a7ed1e0b18cf5a6324fcbf6
L7 <1525804307@qq.com>
2022-10-20 20:30:26 +0800

    373 和最小的k对数字

--- resolve/src/KSmallestPairs_373.java
+++ resolve/src/KSmallestPairs_373.java
@@ -1,14 +1,30 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class KSmallestPairs_373 {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        int i = 0, j = 0;
-        while (ans.size() < k && (i < nums1.length || j < nums2.length)) {
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(k, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
+            }
+        });
+        for (int i = 0; i < Math.min(k, nums1.length); i++) {
+            priorityQueue.add(new int[]{i, 0});
+        }
+        while (ans.size() < k && !priorityQueue.isEmpty()){
+            int []min = priorityQueue.poll();
             List<Integer> subAns = new ArrayList<>();
-            subAns.add(nums1[i]);
-            subAns.add(nums2[j]);
+            subAns.add(nums1[min[0]]);
+            subAns.add(nums2[min[1]]);
+            ans.add(subAns);
+            if (min[1] + 1 >= nums2.length) {
+                continue;
+            }
+            priorityQueue.add(new int[]{min[0], min[1] + 1});
         }
         return ans;
     }
