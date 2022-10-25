363ba841c557bfb16e4bef12ba0a7d612928c15c
L7 <1525804307@qq.com>
2022-10-25 21:03:51 +0800

    380 O(1)的插入删除和随机获取

--- resolve/src/RandomizedSet_380.java
+++ resolve/src/RandomizedSet_380.java
@@ -1,18 +1,44 @@
-public class RandomizedSet_380 {
-    public RandomizedSet_380() {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
+public class RandomizedSet_380 {
+    private List<Integer> elements;
+    private Map<Integer, Integer> indexes;
+    private Random random;
+    public RandomizedSet_380() {
+        elements = new ArrayList<>();
+        indexes = new HashMap<>();
+        random = new Random();
     }
 
     public boolean insert(int val) {
-        return false;
+        if (indexes.containsKey(val)) {
+            return false;
+        }
+        elements.add(val);
+        indexes.put(val, elements.size() - 1);
+        return true;
     }
 
     public boolean remove(int val) {
-        return false;
+        if (!indexes.containsKey(val)) {
+            return false;
+        }
+        int index = indexes.get(val);
+        int last = elements.get(elements.size() - 1);
+        elements.set(index, last);
+        elements.remove(elements.size() - 1);
+        indexes.put(last, index);
+        indexes.remove(val);
+
+        return true;
     }
 
     public int getRandom() {
-        return 0;
+        return elements.get(random.nextInt(elements.size()));
     }
 
 }
