cde3590442c91dcfb462ea84a653e671c00eb67d
L7 <1525804307@qq.com>
2022-10-26 20:02:56 +0800

    386 字典序排数

--- resolve/src/LexicalOrder_386.java
+++ resolve/src/LexicalOrder_386.java
@@ -4,6 +4,19 @@ import java.util.List;
 public class LexicalOrder_386 {
 
     public List<Integer> lexicalOrder(int n) {
-        return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            ans.add(num);
+            if (num * 10 <= n) {
+                num = num * 10;
+            } else {
+                while (num % 10 == 9 || num + 1 > n) {
+                    num = num / 10;
+                }
+                num++;
+            }
+        }
+        return ans;
     }
 }
