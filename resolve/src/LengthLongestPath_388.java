3744b438d0dd8809c7732800d13bd93a30aaac3f
L7 <1525804307@qq.com>
2022-10-26 20:28:27 +0800

    388 文件的最长绝对路径

--- resolve/src/LengthLongestPath_388.java
+++ resolve/src/LengthLongestPath_388.java
@@ -1,6 +1,42 @@
+import java.util.Stack;
+
 public class LengthLongestPath_388 {
 
+    public static  void  main(String []args) {
+        System.out.println(new LengthLongestPath_388().lengthLongestPath("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"));
+    }
+
+
     public int lengthLongestPath(String input) {
-        return -1;
+        int max = 0;
+        Stack<Integer> stack = new Stack<>();
+        int pos = 0;
+        while (pos < input.length()) {
+            int deep = 0;
+            while (pos < input.length() && input.charAt(pos) == '\t') {
+                deep++;
+                pos++;
+            }
+            boolean isFile = false;
+            int length = 0;
+            while (pos < input.length() && input.charAt(pos) != '\n') {
+                if (input.charAt(pos) == '.' ){
+                    isFile = true;
+                }
+                length++;
+                pos++;
+            }
+            pos++;
+
+            while (!stack.isEmpty() && stack.size() > deep) {
+                stack.pop();
+            }
+            if (isFile) {
+                max = Math.max((stack.isEmpty() ? 0 : stack.peek()) + length, max);
+            } else {
+                stack.push((stack.isEmpty() ? 0 : stack.peek()) + length + 1);
+            }
+        }
+        return max;
     }
 }
