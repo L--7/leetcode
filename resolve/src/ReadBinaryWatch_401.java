54fa1fc4d10919c4151aae55d88fea74be68eb40
L7 <1525804307@qq.com>
2022-11-08 20:12:00 +0800

    401 二进制手表

---
+++ resolve/src/ReadBinaryWatch_401.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadBinaryWatch_401 {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<>();
+        return ans;
+    }
+
+
+}
