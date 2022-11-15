8d1ae9e4a50fde66e4dfe6c771ab02b10dca084c
L7 <1525804307@qq.com>
2022-11-15 20:49:30 +0800

    319 灯泡开关

---
+++ resolve/src/BulbSwitch_319.java
@@ -0,0 +1,5 @@
+public class BulbSwitch_319 {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
---
+++ resolve/src/CountNodes_222.java
@@ -0,0 +1,5 @@
+public class CountNodes_222 {
+    public int countNodes(TreeNode root) {
+        return -1;
+    }
+}
