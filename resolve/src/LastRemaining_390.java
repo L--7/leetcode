5885927741a351d41045b2d8c4418061a0d68c27
L7 <1525804307@qq.com>
2022-11-17 19:46:13 +0800

    390 消除游戏

--- resolve/src/LastRemaining_390.java
+++ resolve/src/LastRemaining_390.java
@@ -1,5 +1,23 @@
 public class LastRemaining_390 {
+
+    public static void main(String[] args) {
+        System.out.println(new LastRemaining_390().lastRemaining(9));
+    }
+
     public int lastRemaining(int n) {
-        return n;
+        int step = 1;
+        int start = 1;
+        int k = 0;
+        while (n > 1) {
+            if (k % 2 == 0) {
+                start = start + step;
+            } else {
+                start = n % 2 == 0 ? start : start + step;
+            }
+            n = n / 2;
+            step = step * 2;
+            k++;
+        }
+        return start;
     }
 }
